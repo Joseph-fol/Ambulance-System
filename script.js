7077ed32904f49d8ff9453c7c713643b3420738b
Olawoyin Joseph Folasakin <olawoyinjoseph05@gmail.com>
2026-02-17 16:02:49 +0100

    Fix feedback section ID and update form submission logic

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const inputName = document.getElementById("inputName").value
-const inputAddress = document.getElementById('inputAddress').value
-const inputType = document.getElementById('inputType').value
+const inputName = document.getElementById("inputName")
+const inputAddress = document.getElementById('inputAddress')
+const inputType = document.getElementById('inputType')
 
 const ringingTone = new Audio('marimba-ringtone.wav')
 
 
 function btnDial() {
     ringingTone.play();
-
     const incomingCallAlert = document.getElementById("incomingCallAlert")
+
     incomingCallAlert.style.display = "block"
 }
 
@@ -38,35 +38,55 @@ const agentReviewPanel = document.getElementById('agentReviewPanel');
 const displayName = document.getElementById('displayName')
 const displayAddress = document.getElementById('displayAddress')
 const displayType = document.getElementById('displayType')
+const emptyInputAlert = document.getElementById('emptyInputAlert')
+const btnAccept = document.getElementById('btn-accept')
+const btnReject = document.getElementById('btn-reject')
 
 function submit(){
-    if (inputName == "" || inputAddress == "" || inputType == "select") {
+    if (inputName.value == "" || inputAddress.value == "" || inputType.value == "select") {
         emptyInputAlert.style.display = "block"
     } else {
-        emptyInputAlert.style.display = "block"
+        emptyInputAlert.style.display = "none"
         agentReviewPanel.style.display = "block"
+
         displayName.innerText = inputName.value
         displayAddress.innerText = inputAddress.value
         displayType.innerText = inputType.value
+        intakeFormSection.style.display = "block"
         incomingCallAlert.innerText = "Form Received"
+        
+        
+        btnAccept.style.display = "none"
+        btnReject.style.display = "none"
+        
+        inputName.value = ''
+        inputAddress.value = ''
+        inputType.value = 'select'
     }
-
-    inputName.value = ''
-    inputAddress.value = ''
-    inputType.value = ''
 }
+
+const feedbackSection = document.getElementById('feedbackSection')
 
 function approve() {
     intakeFormSection.innerHTML = ""
     intakeFormSection.innerHTML = `<p class="alert alert-success text-center fw-bold text-black">Your emergency has been approved, kindly wait till we reach you.</p>`
-    // intakeFormSection.innerHTML = ""
+    agentReviewPanel.style.display = "none"
+    feedbackSection.style.display = "block"
 }
 
 function pending() {
+    agentReviewPanel.style.display = "none"
     intakeFormSection.innerHTML = ""
     intakeFormSection.innerHTML = `<p class="alert alert-success text-center fw-bold text-black">Your emergency is on pending</p>`
 }
 
 function deny() {
+    agentReviewPanel.style.display = "none"
     intakeFormSection.innerHTML = `<p class="alert alert-success text-center fw-bold text-black">Rejected</p>`
+}
+
+function btnRestart(){
+    intakeFormSection.innerHTML = ""
+    feedbackSection.style.display = "none"
+    incomingCallAlert.innerText = "Reload your page"
 }
